bebb14da822925a7b53aab7e7aeb03a26fae91a5
apolisskaya <apolisskaya2016@fau.edu>
2018-03-07 05:51:46 -0500

    modified code to use only relevant transaction types, added confusion matrix printout after logistic regression model usage, cleaned up code

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from sklearn import datasets, model_selection as ms
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def load_in_predictor_and_target_data(df, predictor_variable_list, target_variable_list):
+    predictor_data, target = [], []
+    for row in df[predictor_variable_list].values:
+        predictor_data.append(row)
+    for row in df[target_variable_list].values:
+        target.append(row[0])
+    return predictor_data, target
+
+
+allowable_transaction_types = ['TRANSFER', 'CASH_OUT']
+# only TRANSFER and CASH_OUT are ever fraud, so only take those points
 df = pd.read_csv("paysim_kaggle_dataset.csv")
+df = df.loc[df['type'].isin(allowable_transaction_types)]
+predictor_vars = ['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest']
+target_vars = ['isFraud']
 
-# data: contains the values
-# target names: the names of the target variables
-# .data is a list of lists, each of the same length, containing the predictor variables
-# .target is the possible labels, or response variables, in order in one list
-
-paysim_predictor_data = []
-paysim_target = []
-
-# load data into predictor and target data lists
-for row in df[['isFraud']].values:
-    paysim_target.append(row[0])
-for row in df[['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest']].values:
-    paysim_predictor_data.append(row)
+paysim_predictor_data, paysim_target = load_in_predictor_and_target_data(df, predictor_vars, target_vars)
 
 x_train, x_test, y_train, y_test = ms.train_test_split(paysim_predictor_data, paysim_target, test_size=0.25, random_state=None)
 logisticRegr = LogisticRegression()  # instance of the logistic regression model with all params as default
@@ -26,6 +30,14 @@ logisticRegr.fit(x_train, y_train)  # train the model with 75% of our data
 
 # make predictions on the remaining 25% of the data
 predictions = logisticRegr.predict(x_test)
+
+"""
+--Confusion Matrix Formatting--
+           [ True Positives ] [ False Positives / Type I ]
+[ False Negatives / Type II ] [ True Negatives ]
+"""
+confusion_matrix = confusion_matrix(y_test, predictions)
+print('confusion matrix\n', confusion_matrix)
 
 # check accuracy of the model / performance
 score = logisticRegr.score(x_test, y_test)
